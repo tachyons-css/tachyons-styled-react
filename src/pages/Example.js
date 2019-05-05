621ca8b28122b34a4287c3d23f662c1e37becb3c
mrmrs <hi@mrmrs.cc>
2019-05-05 18:31:40 -0400

    Add example page for copy paste

---
+++ src/pages/Example.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import jsx from '../jsx'
+import React from 'react'
+import theme from '../theme'
+
+function Page() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Page
